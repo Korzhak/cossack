4db214cfd179ea2d55733ea59c382808f3727a60
Bohdan <navchalkin@ex.ua>
2019-06-23 18:49:55 +0300

    added script for getting data from thermostat

---
+++ plugins/thermostat/thermostat_manager.py
@@ -0,0 +1,49 @@
+import socket
+import re
+
+pattern = r"([\d.]+)"
+SOCK_HOST = 'localhost'
+SOCK_PORT = 10001
+
+GET_DATA = "get_data"
+
+
+class ThermostatBox:
+    def __init__(self):
+        # Create a TCP/IP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = (SOCK_HOST, SOCK_PORT)
+
+        self.sock.connect(server_address)
+
+    def send_data_to_box(self, text):
+        try:
+            self.sock.sendall(text.encode())
+
+        finally:
+            self.sock.close()
+
+    def get_data_from_box(self):
+        try:
+
+            # Send data
+            message = GET_DATA.encode()
+
+            self.sock.sendall(message)
+
+            # Look for the response
+            amount_received = 0
+            amount_expected = len(message)
+
+            while amount_received < amount_expected:
+                data = self.sock.recv(1024)
+                amount_received += len(data)
+                print(data.decode())
+                res = re.findall(pattern, data.decode())
+                print(res)
+
+        finally:
+
+            self.sock.close()
