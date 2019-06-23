30bed0f23bdddb81c50fcd3fa37cfec1b7290be8
Bohdan <navchalkin@ex.ua>
2019-06-23 19:58:32 +0300

    added text to message with data from box

--- plugins/thermostat/thermostat.py
+++ plugins/thermostat/thermostat.py
@@ -18,5 +18,10 @@ class Thermostat:
 
     def get_data(self, bot, update):
         text = self.ts.get_data_from_box()
+        formatting_text = f"Thermostat: {'On' if int(text[0]) else 'Off'}\n" \
+                          f"Current state: {text[1]}\n" \
+                          f"Temperature: {text[2]}\n" \
+                          f"Set temp.: {text[3]}\n" \
+                          f"Light: {'On' if int(text[6]) else 'Off'}\n"
         bot.send_message(chat_id=update.message.chat_id,
-                         text=text)
+                         text=formatting_text, parse_mode="HTML")
